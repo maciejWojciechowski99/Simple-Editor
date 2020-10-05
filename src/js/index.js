f02b4fbff211701265dc1516f38f302d81356619
Maciej Wojciechowski <maciejwojciechowskiofc@gmail.com>
2020-10-05 14:13:08 +0200

    new background

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,30 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+
+
+const textarea = document.querySelector('.entry--js');
+const save = document.querySelector('.button__save--js');
+const load = document.querySelector('.button__load--js');
+
+const currentValue = localStorage.getItem('entry');
+
+if(currentValue) {
+    document.querySelector('.info--js').innerHTML = "🥐";
+}
+
+save.addEventListener('click', (e) => {
+    e.preventDefault();
+    localStorage.setItem('entry', textarea.value);
+    if(textarea.value){
+        document.querySelector('.info--js').innerHTML = "🥐";
+    }else {
+        document.querySelector('.info--js').innerHTML = "";
+    }
+})
+
+
+load.addEventListener('click', (e) => {
+    e.preventDefault();
+    textarea.value = localStorage.getItem('entry');
+})
